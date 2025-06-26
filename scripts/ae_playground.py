213ba8822390f15a0512adb372b9c149d7476267
Manuel Wegmann <manuelw@alvis1.int.private>
2025-06-26 18:44:29 +0200

    Working on AE.

--- scripts/ae_playground.py
+++ scripts/ae_playground.py
@@ -4,21 +4,31 @@ from medvae import MVAE
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import torchio as tio
 
-fpath = "/mimer/NOBACKUP/groups/brainage/data/oasis3/derivatives/mriprep/sub-OAS30001/ses-d0129/sub-OAS30001_ses-d0129_space-MNI152NLin2009cAsym_desc-brain_T1w.nii.gz"
+fpath = "/mimer/NOBACKUP/groups/brainage/thesis_brainage/data/sub-OAS30001/ses-d0757/sub-OAS30001_ses-d0757_latent_8.pt"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = MVAE(model_name="medvae_4_1_3d", modality="mri").to(device)
-print("model loaded, will now be applied.")
-img = model.apply_transform(fpath).to(device)
+# Load raw 3D volume
+tensor = torch.load(fpath) 
 
-model.requires_grad_(False)
-model.eval()
+print(tensor.shape)
 
-print("Check.")
-with torch.no_grad():
-    decoded_img, latent = model(img, decode=True)
+tensor = tensor.unsqueeze(0)
 
-print("Ran model. Now saving latent representation.")
+print(tensor.shape)
 
-torch.save(latent.cpu(), '/mimer/NOBACKUP/groups/brainage/thesis_brainage/results/latent.pt')
+# Create dummy affine (identity or your known affine)
+affine = torch.eye(4)
+
+# Wrap in ScalarImage
+image = tio.ScalarImage(tensor=tensor, affine=affine)
+
+# Resize to desired shape (e.g., [128, 128, 128])
+resized = tio.Resize((24, 24, 24))(image)
+
+print(resized.shape)
+
+resized = resized.data
+
+print(resized.shape)
